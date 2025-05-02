33f1410d8044fe44939070e4dae4eec8c1f62010
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-02 14:37:04 +0530

    Update slash.h

--- actor class/Actor_Header/slash.h
+++ actor class/Actor_Header/slash.h
@@ -1,12 +1,13 @@
-#pragma once
+ #pragma once
  
  #include "CoreMinimal.h"
  
- 
  #define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
- // this will draw a sphere at the location with a radius of 25 units and 12 segments.
- 
-
-// new code than previous code: 
  #define DRAW_LINE(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
- // this will draw a line from StartLocation to EndLocation with a color of red and a thickness of 1 unit.
+ #define DRAW_LINE(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
+ #define DRAW_POINT(Location) if (GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Red, true);
+ #define DRAW_VECTOR(StartLocation, EndLocation) if (GetWorld()) \
+ 	{ \
+ 		DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f); \
+ 		DrawDebugPoint(GetWorld(), EndLocation, 15.f, FColor::Red, true); \
+ 	}
