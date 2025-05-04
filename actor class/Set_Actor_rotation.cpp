639ea35659cba6928e951135b38ccd763bb8a29e
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-04 11:11:36 +0530

    Create Set_Actor_rotation.cpp

---
+++ actor class/Set_Actor_rotation.cpp
@@ -0,0 +1,29 @@
+#include "Items/Item.h"
+#include "Slash/DebugMacros.h"
+ 
+AItem::AItem()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+ 
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+ 
+ 	UWorld* World = GetWorld();
+ 
+ 	SetActorLocation(FVector(0.f, 0.f, 50.f));
+ 	SetActorRotation(FRotator(0.f, 45.f, 0.f));
+ 	FVector Location = GetActorLocation();
+ 	FVector Forward = GetActorForwardVector();
+ 	DRAW_SPHERE(Location);
+ 	//DRAW_LINE(Location, Location + Forward * 100.f);
+ 	//DRAW_POINT(Location + Forward * 100.f);
+ 	DRAW_VECTOR(Location, Location + Forward * 100.f);
+ 
+}
+ 
+void AItem::Tick(float DeltaTime)
+{
+ 	Super::Tick(DeltaTime);
+}
