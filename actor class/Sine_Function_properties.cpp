d6013fa44c940755fcf91d9697392dfa7e7183fd
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-05 11:20:47 +0530

    Create Sine_Function_properties.cpp

---
+++ actor class/Sine_Function_properties.cpp
@@ -0,0 +1,28 @@
+#include "Items/Item.h"
+#include "Slash/DebugMacros.h"
+ 
+AItem::AItem()
+{
+ 	PrimaryActorTick.bCanEverTick = true;
+}
+ 
+void AItem::BeginPlay()
+{
+    Super::BeginPlay();
+}
+ 
+void AItem::Tick(float DeltaTime)
+{
+ 	Super::Tick(DeltaTime);
+ 
+
+ 	RunningTime += DeltaTime;
+ 
+ 	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
+ 
+ 	AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
+ 
+
+ 	DRAW_SPHERE_SingleFrame(GetActorLocation());
+ 	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
+}
