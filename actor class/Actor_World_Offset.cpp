22022c220d4b3d3b71699134222e0cc4519b15b2
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-05 11:15:26 +0530

    Create Actor_World_Offset.cpp

---
+++ actor class/Actor_World_Offset.cpp
@@ -0,0 +1,27 @@
+#include "Items/Item.h"
+#include "Slash/DebugMacros.h"
+ 
+AItem::AItem()
+{
+ 	PrimaryActorTick.bCanEverTick = true;
+}
+ 
+ void AItem::BeginPlay()
+{
+ 	Super::BeginPlay();
+}
+ 
+void AItem::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    // Movement rate in units of cm/s
+    float MovementRate = 50.f; 
+    float RotationRate = 45.f;
+
+    // MovementRate * DeltaTime (cm/s) * (s/frame) = (cm/frame)
+    AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
+    AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
+    DRAW_SPHERE_SingleFrame(GetActorLocation());
+    DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
+}
