ee059ef8f41abc28cec0de70a04ae0c878ac1222
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-06 14:43:02 +0530

    Update item.h

--- actor class/Actor_Header/item.h
+++ actor class/Actor_Header/item.h
@@ -20,11 +20,14 @@ protected:
 private:
  	// provide sine wave movement to the actor.
  	// Movement rate in units of cm/s
+
+	// The visible anywhere macro is used to expose the variable to the editor.
+	UPROPERTY(visibleAnywhere)
  	float RunningTime;
-	// the UPROPERTY macro is used to expose the variable to the editor.
-	UPROPERTY(EditAnywhere)
+	// the UPROPERTY macro is used to expose the variable to the editor and make it visible to blueprints.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
  	float Amplitude = 0.25f;
-	// the UPROPERTY macro is used to expose the variable to the editor.
-	UPROPERTY(EditAnywhere)
+	// the UPROPERTY macro is used to expose the variable to the editor and make it visible to blueprints.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
  	float TimeConstant = 5.f;
 };
