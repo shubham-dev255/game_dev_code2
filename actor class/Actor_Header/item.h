9cd8ccd58d60bc19dfdf291b04caafe3a01113da
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-06 14:49:32 +0530

    Update item.h

--- actor class/Actor_Header/item.h
+++ actor class/Actor_Header/item.h
@@ -16,7 +16,21 @@ public:
 
 protected:
 	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
+	// the UPROPERTY macro is used to expose the variable to the editor and make it visible to event graphs.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+	float Amplitude = 0.25f;
+	// the UPROPERTY macro is used to expose the variable to the editor and make it visible to event graph.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+	float TimeConstant = 5.f;
+
+	UFUNCTION(BlueprintPure)
+ 	float TransformedSin();
+ 
+ 	UFUNCTION(BlueprintPure)
+ 	float TransformedCos();
+	
 private:
  	// provide sine wave movement to the actor.
  	// Movement rate in units of cm/s
@@ -24,10 +38,5 @@ private:
 	// The visible anywhere macro is used to expose the variable to the editor.
 	UPROPERTY(visibleAnywhere)
  	float RunningTime;
-	// the UPROPERTY macro is used to expose the variable to the editor and make it visible to event graphs.
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
- 	float Amplitude = 0.25f;
-	// the UPROPERTY macro is used to expose the variable to the editor and make it visible to event graph.
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
- 	float TimeConstant = 5.f;
+
 };
