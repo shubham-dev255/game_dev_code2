758cebc5d6b2e2569b69b36e175235616db07a66
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-08 15:08:03 +0530

    Create Bird.h

---
+++ Pawn_Class/Item/Header_File/item/Bird.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Bird.generated.h"
+
+UCLASS()
+class SLASH_API ABird : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ABird();
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+};
