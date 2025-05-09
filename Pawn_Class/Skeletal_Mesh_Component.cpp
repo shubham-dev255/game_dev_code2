edc10a9967aac642a80b33b372cef0d43318130e
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-09 15:35:27 +0530

    Create Skeletal_Mesh_Component.cpp

---
+++ Pawn_Class/Skeletal_Mesh_Component.cpp
@@ -0,0 +1,33 @@
+#include "Pawns/Bird.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+ABird::ABird()
+{
+@@ -12,24 +13,27 @@
+	Capsule->SetCapsuleHalfHeight(20.f);
+	Capsule->SetCapsuleRadius(15.f);
+	SetRootComponent(Capsule);
+
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
+	BirdMesh->SetupAttachment(GetRootComponent());
+}
+
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
