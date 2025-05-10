841804311a705b11d59fc6b75d47f96f60f17618
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-10 12:22:36 +0530

    Update Bird.h

--- Pawn_Class/Header_file/Bird.h
+++ Pawn_Class/Header_file/Bird.h
@@ -8,6 +8,8 @@
 
 class UCapsuleComponent;
 class USkeletalMeshComponent;
+class USpringArmComponent;
+class UCameraComponent;
 
 UCLASS()
 class SLASH_API ABird : public APawn
@@ -31,5 +33,11 @@ private:
 
 	UPROPERTY(VisibleAnywhere)
 	USkeletalMeshComponent* BirdMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* ViewCamera;
 	
 };
