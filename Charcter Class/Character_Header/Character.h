4dfc4633950b0796dbd1cfba50bd22823cc7c758
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-11 11:26:56 +0530

    Update Character.h

---
+++ Charcter Class/Character_Header/Character.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SlashCharacter.generated.h"
+
+UCLASS()
+class SLASH_API ASlashCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASlashCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+};
