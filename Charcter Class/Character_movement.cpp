5e91d49c8f3782311a12aba472da66c6628cf56e
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-12 11:50:22 +0530

    Create Character_movement.cpp

---
+++ Charcter Class/Character_movement.cpp
@@ -0,0 +1,35 @@
+#include "Characters/SlashCharacter.h"
+
+ASlashCharacter::ASlashCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ASlashCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ASlashCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(FName("MoveForward"), this, &ASlashCharacter::MoveForward);
+}
+
+void ASlashCharacter::MoveForward(float Value)
+{
+	if (Controller && (Value != 0.f))
+	{
+		FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, Value);
+	}
+}
