ba7d48690605ffa02cde6ad489f78381fd4d7bb5
shubham-dev255 <shubhamshri2505@gmail.com>
2025-05-20 14:51:42 +0530

    Update Weapon.h

---
+++ Weapon_Class/Header_File/Weapon.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+protected:
+	
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+};
