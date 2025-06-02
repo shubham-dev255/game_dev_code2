15b5cf80c56e784e770a400c6449e8bec795253b
shubham-dev255 <shubhamshri2505@gmail.com>
2025-06-02 13:38:25 +0530

    Update Weapon.h

--- Weapon_Class/Header_File/Weapon.h
+++ Weapon_Class/Header_File/Weapon.h
@@ -4,9 +4,8 @@
 #include "Items/Item.h"
 #include "Weapon.generated.h"
 
-/**
- * 
- */
+class USoundBase;
+
 UCLASS()
 class SLASH_API AWeapon : public AItem
 {
@@ -19,4 +18,8 @@ protected:
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
 
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
+	USoundBase* EquipSound;
 };
