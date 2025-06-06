3b5abc64092f08989a0ea38eb291988d0c0d3045
shubham-dev255 <shubhamshri2505@gmail.com>
2025-06-06 16:33:06 +0530

    Update Weapon.h

--- Weapon_Class/Header_File/Weapon.h
+++ Weapon_Class/Header_File/Weapon.h
@@ -16,10 +16,11 @@ public:
 	void Equip(USceneComponent* InParent, FName InSocketName);
 	void AttachMeshToSocket(USceneComponent* InParent, const FName& InSocketName);
 protected:
-	
+	virtual void BeginPlay() override;
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
-
+	UFUNCTION()Add commentMore actions
+	void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 private:
 
 	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
