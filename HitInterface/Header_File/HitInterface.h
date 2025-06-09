1a61673e4390431535e3be6c7a73745da22af786
shubham-dev255 <shubhamshri2505@gmail.com>
2025-06-09 17:15:08 +0530

    Update HitInterface.h

--- HitInterface/Header_File/HitInterface.h
+++ HitInterface/Header_File/HitInterface.h
@@ -1,6 +1,27 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-Add commentMore actions
 
-#include "Interfaces/HitInterface.h"
+// Fill out your copyright notice in the Description page of Project Settings.Add commentMore actions
 
-// Add default functionality here for any IHitInterface functions that are not pure virtual.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "HitInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UHitInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class SLASH_API IHitInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	virtual void GetHit(const FVector& ImpactPoint) = 0;
+};
