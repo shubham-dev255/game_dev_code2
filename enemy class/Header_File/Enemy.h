7494f3bb60317a72422322fe3c53bf48016fbc3e
shubham-dev255 <shubhamshri2505@gmail.com>
2025-06-09 17:13:04 +0530

    Create Enemy.h

---
+++ enemy class/Header_File/Enemy.h
@@ -0,0 +1,22 @@
+#include "CoreMinimal.h"Add commentMore actions
+#include "GameFramework/Character.h"
+#include "Interfaces/HitInterface.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class SLASH_API AEnemy : public ACharacter
+class SLASH_API AEnemy : public ACharacter, public IHitInterface
+{
+	GENERATED_BODY()
+
+@@ -17,9 +18,10 @@
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void GetHit(const FVector& ImpactPoint) override;
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+
+};
