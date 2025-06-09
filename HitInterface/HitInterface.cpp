ed24a71c37916bb436df5635e2106dc5f61c3d36
shubham-dev255 <shubhamshri2505@gmail.com>
2025-06-09 17:15:14 +0530

    Update HitInterface.cpp

--- HitInterface/HitInterface.cpp
+++ HitInterface/HitInterface.cpp
@@ -1,26 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.Add commentMore actions
+// Fill out your copyright notice in the Description page of Project Settings.
 
-#pragma once
+#include "Interfaces/HitInterface.h"
 
-#include "CoreMinimal.h"
-#include "UObject/Interface.h"
-#include "HitInterface.generated.h"
-
-// This class does not need to be modified.
-UINTERFACE(MinimalAPI)
-class UHitInterface : public UInterface
-{
-	GENERATED_BODY()
-};
-
-/**
- * 
- */
-class SLASH_API IHitInterface
-{
-	GENERATED_BODY()
-
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
-public:
-	virtual void GetHit() = 0;
-};
+// Add default functionality here for any IHitInterface functions that are not pure virtual.
