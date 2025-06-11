1099fed69dba83fc747825e620dcd01bbfa338ab
shubham-dev255 <shubhamshri2505@gmail.com>
2025-06-11 19:34:03 +0530

    Update Enemy.h

--- enemy class/Header_File/Enemy.h
+++ enemy class/Header_File/Enemy.h
@@ -3,18 +3,32 @@
 #include "Interfaces/HitInterface.h"
 #include "Enemy.generated.h"
 
+class UAnimMontage;
+
 UCLASS()
 
 class SLASH_API AEnemy : public ACharacter, public IHitInterface
 {
 	GENERATED_BODY()
 
-@@ -17,9 +18,10 @@
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+
+	/**
+	* Animation montages
+	*/
+	UPROPERTY(EditDefaultsOnly, Category = Montages)
+	UAnimMontage* HitReactMontage;
+
 	virtual void GetHit(const FVector& ImpactPoint) override;
 protected:
+
+	/**
+	* Play montage functions
+	*/
+	void PlayHitReactMontage(const FName& SectionName);
 	virtual void BeginPlay() override;
 
 public:	
