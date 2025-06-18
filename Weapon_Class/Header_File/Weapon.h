16b89022a7a1ecefd5179df5f03b19a780a5b4a1
shubham-dev255 <shubhamshri2505@gmail.com>
2025-06-18 18:24:54 +0530

    Update Weapon.h

--- Weapon_Class/Header_File/Weapon.h
+++ Weapon_Class/Header_File/Weapon.h
@@ -15,6 +15,7 @@ public:
 	AWeapon();
 	void Equip(USceneComponent* InParent, FName InSocketName);
 	void AttachMeshToSocket(USceneComponent* InParent, const FName& InSocketName);
+	TArray<AActor*> IgnoreActors;
 protected:
 
 	virtual void BeginPlay() override;
