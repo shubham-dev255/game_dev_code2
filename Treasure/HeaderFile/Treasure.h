35b81d6417a683bd69c319e6cb86ea94f424f2f4
shubham-dev255 <shubhamshri2505@gmail.com>
2025-07-08 19:45:17 +0530

    Update Treasure.h

---
+++ Treasure/HeaderFile/Treasure.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Treasure.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API ATreasure : public AItem
+{
+	GENERATED_BODY()
+protected:
+
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+private:
+
+	UPROPERTY(EditAnywhere, Category = Sounds)
+	USoundBase* PickupSound;
+};
