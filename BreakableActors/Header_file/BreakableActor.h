b078729d55a05dd34376fac19be45c026f78c33b
shubham-dev255 <shubhamshri2505@gmail.com>
2025-07-09 23:02:03 +0530

    Update BreakableActor.h

--- BreakableActors/Header_file/BreakableActor.h
+++ BreakableActors/Header_file/BreakableActor.h
@@ -21,7 +21,16 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(VisibleAnywhere)
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	UGeometryCollectionComponent* GeometryCollection;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UCapsuleComponent* Capsule;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Breakable Properties")
+	TSubclassOf<class ATreasure> TreasureClass;
+
 };
