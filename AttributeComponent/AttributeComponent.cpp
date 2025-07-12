386269843da9911204931ebe18d3e7c4a5fd5dcd
shubham-dev255 <shubhamshri2505@gmail.com>
2025-07-12 14:54:49 +0530

    Update AttributeComponent.cpp

---
+++ AttributeComponent/AttributeComponent.cpp
@@ -0,0 +1,20 @@
+#include "Components/AttributeComponent.h"
+
+UAttributeComponent::UAttributeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	
+}
+
+void UAttributeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void UAttributeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
