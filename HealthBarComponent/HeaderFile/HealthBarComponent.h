ca8ef38e756652af5dc19ed406c27083d0432610
shubham-dev255 <shubhamshri2505@gmail.com>
2025-07-13 17:40:23 +0530

    Update HealthBarComponent.h

---
+++ HealthBarComponent/HeaderFile/HealthBarComponent.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "HealthBarComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API UHealthBarComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+	
+};
