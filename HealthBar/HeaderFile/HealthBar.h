6fa8a620e8907c1de274c665eb55a4dc1ee7a509
shubham-dev255 <shubhamshri2505@gmail.com>
2025-07-14 17:20:35 +0530

    Update HealthBar.h

---
+++ HealthBar/HeaderFile/HealthBar.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HealthBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API UHealthBar : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* HealthBar;
+};
