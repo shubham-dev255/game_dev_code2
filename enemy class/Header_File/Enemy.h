34828cfd0b019bd98b6e51cdc10cc50bdacb6d8f
shubham-dev255 <shubhamshri2505@gmail.com>
2025-08-05 20:27:18 +0530

    Update Enemy.h

--- enemy class/Header_File/Enemy.h
+++ enemy class/Header_File/Enemy.h
@@ -18,6 +18,8 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	virtual void GetHit_Implementation(const FVector& ImpactPoint) override;
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
+	virtual void Destroyed() override;
 private:
 
 
