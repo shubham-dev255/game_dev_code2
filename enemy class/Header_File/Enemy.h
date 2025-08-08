ded30ef386d510fe324a97e97e4c85631dd8e809
shubham-dev255 <shubhamshri2505@gmail.com>
2025-08-08 16:12:50 +0530

    Update Enemy.h

--- enemy class/Header_File/Enemy.h
+++ enemy class/Header_File/Enemy.h
@@ -67,6 +67,9 @@ protected:
 	void MoveToTarget(AActor* Target);
 	AActor* ChoosePatrolTarget();
 
+	virtual void Attack() override;
+	virtual void PlayAttackMontage() override;
+
 	UFUNCTION()
 	void PawnSeen(APawn* SeenPawn);
 
