1d916ecb90e054fcd8d34b959cd41d3c7c56da5e
shubham-dev255 <shubhamshri2505@gmail.com>
2025-08-10 20:49:30 +0530

    Update Character.h

--- Charcter Class/Character_Header/Character.h
+++ Charcter Class/Character_Header/Character.h
@@ -22,6 +22,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	virtual void GetHit_Implementation(const FVector& ImpactPoint) override;
 protected:
 	virtual void BeginPlay() override;
 
